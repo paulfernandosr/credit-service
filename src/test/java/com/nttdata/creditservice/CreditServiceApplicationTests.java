3c83d5ce20a98b3ee7e14e70ccd7031ed9ce820c
Paul Fernando Sacaca Ramirez <pfsr98@gmail.com>
2022-07-19 18:51:23 -0500

    update for project 3

--- src/test/java/com/nttdata/creditservice/CreditServiceApplicationTests.java
+++ src/test/java/com/nttdata/creditservice/CreditServiceApplicationTests.java
@@ -1,12 +1,10 @@
 package com.nttdata.creditservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CreditServiceApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
