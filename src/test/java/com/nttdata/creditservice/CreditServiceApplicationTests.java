5ae1322d9067c18b016655120e2bb89a92547ef3
Paul Fernando Sacaca Ramirez <pfsr98@gmail.com>
2022-07-21 07:55:26 -0500

    added @Test

--- src/test/java/com/nttdata/creditservice/CreditServiceApplicationTests.java
+++ src/test/java/com/nttdata/creditservice/CreditServiceApplicationTests.java
@@ -1,10 +1,12 @@
 package com.nttdata.creditservice;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CreditServiceApplicationTests {
 
+	@Test
 	void contextLoads() {
 	}
 
